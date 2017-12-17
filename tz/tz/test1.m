24070f26333b8c0f165db812d2e943ba0d42da75
zcs <625834120@qq.com>
2017-12-17 21:45:50 +0800

    写了test1

---
+++ tz/tz/test1.m
@@ -0,0 +1,21 @@
+//
+//  test1.m
+//  tz
+//
+//  Created by 阿K on 2017/12/17.
+//  Copyright © 2017年 阿K. All rights reserved.
+//
+
+#import "test1.h"
+
+@implementation test1
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
