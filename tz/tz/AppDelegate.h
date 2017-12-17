fe2f6efcbc60b4d19f305d0830aa74589cd6d3c5
zcs <625834120@qq.com>
2017-12-17 21:32:26 +0800

    s123

---
+++ tz/tz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tz
+//
+//  Created by 阿K on 2017/12/17.
+//  Copyright © 2017年 阿K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tz/tz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tz
+//
+//  Created by 阿K on 2017/12/17.
+//  Copyright © 2017年 阿K. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
